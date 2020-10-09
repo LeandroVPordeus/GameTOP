ea71ae9c7fb0d4a50e9ae06f36227f621f77f58f
LeandroVPordeus <58612986+LeandroVPordeus@users.noreply.github.com>
2020-10-08 23:51:49 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GameTOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var jogo = new JogoFODA();
+            jogo.IniciarJogo();
+        }
+    }
+
+    class JogoFODA
+    {
+        public JogoFODA(string nome)
+        {
+            
+        }
+        public void IniciarJogo()
+        {
+            Console.Write($"{}Jogador deu Um Passe");
+        }
+
+    }
+    
+}
